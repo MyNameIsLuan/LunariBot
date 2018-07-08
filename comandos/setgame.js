8e31e92fa91be66e383240f08bae243ad2ae319f
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-07-08 10:33:30 -0300

    Create setgame.js

---
+++ comandos/setgame.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+   message.delete().catch(O_o=>{})
+        let mensg = args.join(" ");
+        if(!mensg)
+           return message.channel.send("Digite a msg do jogo")
+        client.user.setPresence({ game: { name: `${mensg}`, url: 'https://twitch.tv/LockDzn', type: 1 } });
+        message.reply(`jogo setado para: ${mensg}`)
+    }
