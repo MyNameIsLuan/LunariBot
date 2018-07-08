91733024cf824acb46f4ea8113bc8be9eb9fda07
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-07-08 00:57:42 -0300

    Update staff.js

--- comandos/staff.js
+++ comandos/staff.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{})
-   const stf = new Discord.RichEmbed()
+   const staf = new Discord.RichEmbed()
        .setColor("0cff00")
        .setAuthor("STAFF")
        
@@ -12,6 +12,6 @@ module.exports.run = async (client, message, args) =>{
        .setTimestamp()
        .setFooter(`Comando executado por: ${message.author.tag}`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
     
-    message.channel.send(stf)
+    message.channel.send(staf)
 
 }
