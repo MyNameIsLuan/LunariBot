ba052a6b2e96f4e84d4cffd91807aee746672698
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-07-08 00:54:11 -0300

    Update twitter.js

--- comandos/twitter.js
+++ comandos/twitter.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{})
-   const twitter = new Discord.RichEmbed()
+   const tt = new Discord.RichEmbed()
        .setColor("0cff00")
        .setAuthor("Twitter")
        
@@ -12,6 +12,6 @@ module.exports.run = async (client, message, args) =>{
        .setTimestamp()
        .setFooter(`Comando executado por: ${message.author.tag}`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
     
-    message.channel.send(twitter)
+    message.channel.send(tt)
 
 }
