85b012104da1d215c9f85dd79296f9a62b56f1a8
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-07-23 17:30:41 -0300

    Update staff.js

--- comandos/staff.js
+++ comandos/staff.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) =>{
        .setColor("0cff00")
        .setAuthor("STAFF")
        
-       .setDescription("Nosso FORMULARIO para a staff ATUALMENTE é: \n\nAcesse já: [Clique Aqui](https://goo.gl/forms/cgSjkZEmAN1tT5s33)")
+       .setDescription("SEJA STAFF de nossa rede acessando tais links:\n\nPara Helper: [Clique Aqui](http://bit.ly/formajd)\nPara Trial[Clique Aqui](https://bit.ly/2Lg7XBC)\nPara Builder: [Clique Aqui](http://bit.ly/2hLuFAh)\nPara GC: [Clique Aqui](https://goo.gl/forms/k3CwCxFpHsY7Ubqf1)\n\n")
        
        .setTimestamp()
        .setFooter(`Comando executado por: ${message.author.tag}`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
